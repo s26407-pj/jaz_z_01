a9416086a62eb204f58f8d868c01f6906d80d5f0
s26407-pj <s26407@pjwstk.edu.pl>
2023-03-11 10:22:26 +0100

    first commit

---
+++ src/main/java/pl/pjatk/lukmac/LukmacApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.lukmac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukmacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukmacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/lukmac/LukmacApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.lukmac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LukmacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
